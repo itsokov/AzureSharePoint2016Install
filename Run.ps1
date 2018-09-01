ca58b5823bf4d944536342c2200c6c3a4eb2b32e
itsokov <ivaylo.tsokov@hpe.com>
2018-09-01 23:02:49 +0300

    asd

--- Run.ps1
+++ Run.ps1
@@ -2,14 +2,21 @@
 
 $resourceGroupName="SP2016Dev"
 $location="WestEurope"
-$sqlBinaryUrl=''
 $sharepointBinaryUrl='https://download.microsoft.com/download/0/0/4/004EE264-7043-45BF-99E3-3F74ECAE13E5/officeserver.img'
+$sqlBinaryUrl='https://itsokov.blob.core.windows.net/installblob/SQLServer2016SP2-FullSlipstream-x64-ENU.iso'
 $tempDownloadLocation='C:\temp'
 $storageAccountShareName="assets"
 $randSAName= -join ((97..122) | Get-Random -Count 9 | % {[char]$_})
+$SASKU = 'Premium_LRS'
 $driveToMap='X:'
 $sharepointBinaryLocation="$driveToMap\officeserver.img"
+$sqlBinaryLocation="$driveToMap\SQLServer2016SP2-FullSlipstream-x64-ENU.iso"
 $yourAdminPassword=Read-Host -Prompt "Please enter the password you will use for all accounts"
+$VirtNetName = 'VNPOC1'
+$VMName = -join ((97..122) | Get-Random -Count 9 | % {[char]$_})
+$VMSize ="Standard_DS2"
+$ServerSKU="2016-Datacenter"
+$setupAccount='sp_setup'
 #$autoSPInstallerScriptsUrl='https://github.com/brianlala/AutoSPInstaller/archive/master.zip'
 #endregion
 
@@ -20,24 +27,22 @@ Get-AzureRmSubscription| select -First 1 | Select-AzureRmSubscription
 
 $resourceGroup=New-AzureRmResourceGroup "$resourceGroupName" -Location $location
 
-$storageAcct=New-AzureRmStorageAccount -Name $randSAName -ResourceGroupName $resourceGroupName -SkuName Standard_LRS -Location $location 
+$storageAcct=New-AzureRmStorageAccount -Name $randSAName -ResourceGroupName $resourceGroupName -SkuName $SASKU -Location $location 
 $storageAccountShare=New-AzureStorageShare  -Name $storageAccountShareName  -Context $storageAcct.Context 
-$storkey = Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $randSAName
-#$password=ConvertTo-SecureString -String $storkey -AsPlainText -Force
-#New-SmbMapping -LocalPath X: -RemotePath "\\$randSAName.file.core.windows.net\$storageAccountShareName" -username "$randSAName" -Password $storkey[0].Value
-net use $driveToMap "\\$randSAName.file.core.windows.net\$storageAccountShareName" $storkey[0].Value /user:$randSAName
+$ScriptBlobKey = Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $randSAName
+#$password=ConvertTo-SecureString -String $ScriptBlobKey -AsPlainText -Force
+#New-SmbMapping -LocalPath X: -RemotePath "\\$randSAName.file.core.windows.net\$storageAccountShareName" -username "$randSAName" -Password $ScriptBlobKey[0].Value
+net use $driveToMap "\\$randSAName.file.core.windows.net\$storageAccountShareName" $ScriptBlobKey[0].Value /user:$randSAName
 #New-Item -Path "$driveToMap" -ItemType Directory -Name 'SharePointInstall'
 (New-Object System.Net.WebClient).DownloadFile($sharepointBinaryUrl, $sharepointBinaryLocation)
 
 
 ### download SQL image
-
+(New-Object System.Net.WebClient).DownloadFile($sqlBinaryUrl, $sharepointBinaryLocation)
 
 ### download GitHub Scripts and Config and create folder structure
 #(New-Object System.Net.WebClient).DownloadFile($autoSPInstallerScriptsUrl, "$driveToMap\autospinstaller.zip")
 
-#new-item -Path "$driveToMap" -ItemType Directory "SP\2016\SharePoint\PrerequisiteInstallerFiles"
-#new-item -path "$driveToMap\SP" -ItemType Directory 'AutoSPInstaller'
 
 
 
@@ -52,5 +57,122 @@ $mountIso=Mount-DiskImage -ImagePath "$driveToMap\SQLServer2016SP2-FullSlipstrea
 $isoDriveLetter = ($mountIso | Get-Volume).DriveLetter
 
 Copy-Item -Container "$isoDriveLetter`:" -Destination "$driveToMap\SQLMedia" -Recurse
+Dismount-DiskImage -InputObject $mountIso
 
-### John Savill Part
+#extract SharePoint iso
+
+
+
+#region JohnSavill
+
+
+filter timestamp {"$(Get-Date -Format G): $_"}
+
+#Create Resources for new deployment
+Write-Output "Setting up VM resources and variables" | timestamp
+
+
+#Get latest image
+$AzureImageSku = Get-AzureRmVMImage -Location $location -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus $ServerSKU
+$AzureImageSku = $AzureImageSku | Sort-Object Version -Descending #put the newest first which is the highest patched version
+$AzureImage = $AzureImageSku[0] #Newest
+
+#Create a Virtual Network
+$subnet = New-AzureRmVirtualNetworkSubnetConfig -Name 'StaticSub' -AddressPrefix "10.10.1.0/24"
+$vnet = New-AzureRmVirtualNetwork -Force -Name $VirtNetName -ResourceGroupName $resourceGroupName `
+    -Location $location -AddressPrefix "10.10.0.0/16" -Subnet $subnet # -DnsServer "10.10.1.10" don't set yet
+#If VM points to itself and not offering DNS yet the agents will hang during install
+
+#Create VM
+$vm = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
+#Create NIC
+#For demo for easy access give a public IP
+$pip = New-AzureRmPublicIpAddress -ResourceGroupName $resourceGroupName -Name ('PubIP' + $VMName) `
+    -Location $Location -AllocationMethod Dynamic -DomainNameLabel $vmname.ToLower()
+$nic = New-AzureRmNetworkInterface -Force -Name ('nic' + $VMName) -ResourceGroupName $resourceGroupName `
+    -Location $Location -SubnetId $vnet.Subnets[0].Id -PrivateIpAddress 10.10.1.10 `
+    -PublicIpAddressId $pip.Id
+$vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
+
+$osDiskName = $VMName+'-OSDisk'
+$osDiskCaching = 'ReadWrite'
+$osDiskVhdUri = "https://$randSAName.blob.core.windows.net/vhds/"+$VMName+"-OS.vhd"
+
+# Setup OS & Image
+$user = $setupAccount
+$password = $yourAdminPassword
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($user, $securePassword)  
+$vm = Set-AzureRmVMOperatingSystem -VM $vm -Windows -ComputerName $VMName -Credential $cred
+$vm = Set-AzureRmVMSourceImage -VM $vm -PublisherName $AzureImage.PublisherName -Offer $AzureImage.Offer -Skus $AzureImage.Skus -Version $AzureImage.Version
+$vm = Set-AzureRmVMOSDisk -VM $vm -VhdUri $osDiskVhdUri -name $osDiskName -CreateOption fromImage -Caching $osDiskCaching
+
+$vm = Set-AzureRmVMBootDiagnostics -VM $vm -Disable
+
+#Add two data disks
+$dataDisk1VhdUri = "https://$randSAName.blob.core.windows.net/vhds/"+$VMName+"-Data1.vhd"
+$dataDisk1Name = $VMName+'-data1Disk'
+$vm = Add-AzureRmVMDataDisk -VM $vm -Name $dataDisk1Name -Caching None -CreateOption Empty -DiskSizeInGB 127 -VhdUri $dataDisk1VhdUri -Lun 1
+$dataDisk2VhdUri = "https://$randSAName.blob.core.windows.net/vhds/"+$VMName+"-Data2.vhd"
+$dataDisk2Name = $VMName+'-data2Disk'
+
+$vm = Add-AzureRmVMDataDisk -VM $vm -Name $dataDisk2Name -Caching None -CreateOption Empty -DiskSizeInGB 512 -VhdUri $dataDisk2VhdUri -Lun 2
+
+# Create Virtual Machine
+Write-Output "Creating the VM" | timestamp
+$NewVM = New-AzureRmVM -ResourceGroupName $resourceGroupName -Location $Location -VM $vm 
+Write-Output "VM creation complete" | timestamp
+
+#Now make a DC by running the first boot script
+
+$ScriptBlobURL = "https://$randSAName.blob.core.windows.net/scripts/"
+ 
+$ScriptName = "FirstBoot.ps1"
+$ExtensionName = 'FirstBootScript'
+$ExtensionType = 'CustomScriptExtension' 
+$Publisher = 'Microsoft.Compute'  
+$Version = '1.8'
+$timestamp = (Get-Date).Ticks
+ 
+$ScriptLocation = $ScriptBlobURL + $ScriptName
+$ScriptExe = ".\$ScriptName"
+ 
+$PrivateConfiguration = @{"storageAccountName" = "$$randSAName";"storageAccountKey" = "$ScriptBlobKey"} 
+$PublicConfiguration = @{"fileUris" = [Object[]]"$ScriptLocation";"timestamp" = "$timestamp";"commandToExecute" = "powershell.exe -ExecutionPolicy Unrestricted -Command $ScriptExe"}
+ 
+Write-Output "Injecting First Boot PowerShell" | timestamp
+Set-AzureRmVMExtension -ResourceGroupName $resourceGroupName -VMName $VMName -Location $Location `
+ -Name $ExtensionName -Publisher $Publisher -ExtensionType $ExtensionType -TypeHandlerVersion $Version `
+ -Settings $PublicConfiguration -ProtectedSettings $PrivateConfiguration
+ 
+((Get-AzureRmVM -Name $VMName -ResourceGroupName $resourceGroupName -Status).Extensions | Where-Object {$_.Name -eq $ExtensionName}).Substatuses
+
+Write-Output "Waiting 5 minutes for reboot to complete" | timestamp
+Start-Sleep -Seconds 300 #Wait 5 minutes
+
+#Have to remove the previous before creating a new one
+Remove-AzureRmVMExtension -ResourceGroupName $resourceGroupName -VMName $VMName -Name FirstBootScript -Force
+
+#Now run the second boot script to install SQL and SharePoint
+$ScriptName = "SecondBoot.ps1"
+$ExtensionName = 'SecondBootScript'
+$timestamp = (Get-Date).Ticks
+ 
+$ScriptLocation = $ScriptBlobURL + $ScriptName
+$ScriptExe = ".\$ScriptName"
+ 
+$PrivateConfiguration = @{"storageAccountName" = "$$randSAName";"storageAccountKey" = "$ScriptBlobKey"} 
+$PublicConfiguration = @{"fileUris" = [Object[]]"$ScriptLocation";"timestamp" = "$timestamp";"commandToExecute" = "powershell.exe -ExecutionPolicy Unrestricted -Command $ScriptExe"}
+ 
+Write-Output "Injecting Second Boot PowerShell" | timestamp
+Set-AzureRmVMExtension -ResourceGroupName $resourceGroupName -VMName $VMName -Location $Location `
+ -Name $ExtensionName -Publisher $Publisher -ExtensionType $ExtensionType -TypeHandlerVersion $Version `
+ -Settings $PublicConfiguration -ProtectedSettings $PrivateConfiguration
+ 
+((Get-AzureRmVM -Name $VMName -ResourceGroupName $resourceGroupName -Status).Extensions | Where-Object {$_.Name -eq $ExtensionName}).Substatuses
+
+Remove-AzureRmVMExtension -ResourceGroupName $resourceGroupName -VMName $VMName -Name SecondBootScript -Force
+
+Write-Output "Installation complete" | timestamp
+
+#endregion JohnSavill
